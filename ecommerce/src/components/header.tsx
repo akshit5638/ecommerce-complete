7bb1f73de609b3b5ee438590ed4432990c03441b
Akshit Tyagi <akshit4644@gmail.com>
2024-03-04 23:52:36 +0530

    header

--- ecommerce/src/components/header.tsx
+++ ecommerce/src/components/header.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+
+const user = {
+    _id: "hfgd",
+    role: "admin"
+}
 
 const Header = () => {
+    const [isOpen, setisOpen] = useState<boolean>(false);
+    const logouthandler = () => {
+        setisOpen(false)
+    }
     return (
-        <div>Header</div>
+        <nav className='header'>
+            <Link onClick={() => setisOpen(false)} to={'/'}>Home</Link>
+            <Link onClick={() => setisOpen(false)} to={'/search'}><FaSearch /></Link>
+            <Link onClick={() => setisOpen(false)} to={'/cart'}><FaShoppingBag /></Link>
+
+            {
+                user?._id ? (
+                    <>
+                        <button onClick={() => setisOpen((prev) => !prev)}>
+                            <FaUser />
+                        </button>
+                        <Link onClick={() => setisOpen(false)} to="/admin/dashboard">
+                            Admin
+                        </Link>
+                        <dialog open={isOpen}>
+                            <div>
+
+                                {user.role === "admin" && (
+                                    <Link onClick={() => setisOpen(false)} to="/admin/dashboard">
+                                        Admin
+                                    </Link>
+                                )}
+
+                                <Link onClick={() => setisOpen(false)} to="/orders">
+                                    Orders
+                                </Link>
+                                <button onClick={logouthandler} >
+                                    <FaSignOutAlt />
+                                </button>
+                            </div>
+                        </dialog>
+                    </>
+
+                ) : <Link to={'/login'}><FaSignInAlt /></Link>
+
+            }
+        </nav>
     )
 }
 
